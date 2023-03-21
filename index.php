6e28b2ccc6f8ec2d24c84129d7a1781a25d2d6e5
Olivier F <94624011+OlivierFacelina@users.noreply.github.com>
2023-03-21 12:24:53 +0100

    Lier le XML au PHP

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php $xml = simplexml_load_file('fichier.xml');
 
+// Accéder aux éléments XML
+$titre = $xml->livre->titre;
+$auteur = $xml->livre->auteur;
+
+// Afficher les éléments sur la page 
+echo "Le titre du livre est : $titre";
+echo "L'auteur du livre est : $auteur";
+
 ?>
