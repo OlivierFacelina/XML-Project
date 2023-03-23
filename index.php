ce36788ab06fd98c2fe623b0f00f0af3c5533ed5
HectorD <contactdealh@gmail.com>
2023-03-23 08:49:17 +0100

    add master

--- index.php
+++ index.php
@@ -1,20 +1,13 @@
-<!-- <?php $xml = simplexml_load_file('http://www.ouest-france.fr/rss-en-continu.xml'); 
-// var_dump($xml);
-// Accéder aux éléments XML
-// $titre = $xml->channel->title;
-// // $auteur = $xml->livre->auteur;
+<?php
+$categories = ['Insolite','Politique','Sport','Gastronomie','Culture','Cinéma','Musique','High-tech'];
+$xml = simplexml_load_file('https://www.lepoint.fr/insolite/rss.xml'); 
 
-// // Récupérer les éléments sur la page 
-// echo "Le titre du livre est : $titre";
-// echo "L'auteur du livre est : $auteur";
-
-# Récupérer tous les éléments d'un item
-// foreach ($xml->channel->item as $item) {
-//     $title = $item->title;
-//     echo $title . "<br>";
-// }
-
-?>-->
+$titles = $xml->xpath('//title');
+$descriptions = $xml->xpath('//description');
+$publishDates = $xml->xpath('//pubDate');
+$links = $xml->xpath('//link');
+$imgUrl = $xml->xpath('//enclosure/@url');
+?>
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -25,43 +18,69 @@
     <title>Agregateur</title>
     <link rel="stylesheet" href="./assets/css/style.css">
     <link rel="stylesheet" href="./assets/mediaqueries/mediaqueries.css">
-    <script src="./assets/js/script.js"></script>
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
 </head>
 <body>
     <header>
-    <a id="logo" href=""><img src="./assets/images/logo.png" alt="FluXML Logo" class="logo"></a>
-        <nav class="web-nav">
+        <nav class="web-nav" id="web-nav">
+            <a id="logo" href=""><img src="./assets/images/logo.png" alt="FluXML Logo" class="logo"></a>
             <div class="nav-section">
-                <a href="" class="nav-item"><img src="./assets/images/searchnormal.png" alt="Rechercher" class="nav-icon"></a>
-                <a href="" class="nav-item"><img src="./assets/images/moon.png" alt="Rechercher" class="nav-icon"></a>
-                <a href="" class="nav-item"><img src="./assets/images/heart.png" alt="Rechercher" class="nav-icon"></a>
-                <a href="" class="nav-item"><img src="./assets/images/infocircle.png" alt="Rechercher" class="nav-icon"></a>
+                <a href="" class="nav-item"><img src="./assets/images/searchnormal.png" alt="Rechercher" class="nav-icon" id="search-icon"></a>
+                <a href="" class="nav-item" id="moon"><img src="./assets/images/moon.png" alt="Mode" class="nav-icon" id="moon-icon"></a>
+                <a href="" class="nav-item" id="heart"><img src="./assets/images/heart.png" alt="Rechercher" class="nav-icon" id="heart-emoji"></a>
+                <a href="" class="nav-item"><img src="./assets/images/infocircle.png" alt="Rechercher" class="nav-icon" id="infocircle-icon"></a>
             </div>
         </nav>
 
         <nav class="category-nav">
             <div class="category-hr">
-            </div>
-            <div class="categories">
-                <!-- boucle à faire en php ici -->
-                <h2>Insolite</h2>
-                <h2>Politique</h2>
-                <h2>Sport</h2>
-                <h2>Gastronomie</h2>
-                <h2>Culture</h2>
-                <h2>Cinéma</h2>
-                <h2>Musique</h2>
-                <h2>High-tech</h2>
+                <div class="categories">
+                    <?php for ($i = 0; $i < count($categories); $i++) { ?>
+                        <h2><?= $categories[$i] ?><i class="fa-regular fa-heart"></i></h2>
+                    <?php } ?>
+                </div>
             </div>
         </nav>
     </header>
 
     <section class="web-container">
         <main>
+            <section class="container">
+                <div class="category-upper">
+                    <h2 class="category-title">Insolite</h2>
+                    <a href=""><img src="./assets/images/heart.png" alt="" class="add-favourite" id="heart-category"></a>
+                </div>
+                <hr>
+
+                <section id="articles">
+                    <?php 
+                        for ($i = 2; $i < 10; $i++) {
+                    ?>
+                        <div class="article">
+                            <a href="<?= $links[$i] ?>, " target="blank"><img src="<?= $imgUrl[$i - 2] ?>" alt="" class="article-img"></a>
+                            <div class="article-info">
+                                <div class="article-top">
+                                    <h3 class="article-title"><?= $titles[$i] ?></h3>
+                                    <h4 class="article-desc"><?= $descriptions[$i - 1] ?></h4>
+                                </div>
+                                <h5 class="article-date"><?= $publishDates[$i] ?></h5>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </section>
+            </section>
         </main>
 
         <footer>
         </footer>
     </section>
+    <script src="assets/js/script.js"></script>
+    <script
+      src="https://kit.fontawesome.com/231b981596.js"
+      crossorigin="anonymous"
+    ></script>
 </body>
 </html>
